a0de67d1c1d5cf23322711d8781878a8ca7ab4cd
Arvind Thangamani <dnivra26@gmail.com>
2018-03-13 22:18:16 +0530

    add new relic monitoring

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM golang:1.8
 ADD . /app/ 
 WORKDIR /app 
+RUN go get github.com/prometheus/client_golang/prometheus/promhttp
 RUN go build -o main . 
 CMD ["/app/main"]
--- main.go
+++ main.go
@@ -5,27 +5,40 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/newrelic/go-agent"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	response, err := http.Get("http://time-service")
-	if err != nil {
-		fmt.Printf("%s", err)
-	} else {
-		defer response.Body.Close()
-		contents, err := ioutil.ReadAll(response.Body)
+	if txn, ok := w.(newrelic.Transaction); ok {
+		segment := newrelic.StartSegment(txn, "Go Time call")
+		response, err := http.Get("http://localhost:9091")
+		segment.End()
+
 		if err != nil {
 			fmt.Printf("%s", err)
+		} else {
+			defer response.Body.Close()
+			contents, err := ioutil.ReadAll(response.Body)
+			if err != nil {
+				fmt.Printf("%s", err)
+			}
+			fmt.Fprintf(w, "Hello world!"+string(contents)) // send data to client side
 		}
-		fmt.Fprintf(w, "Hello world!"+string(contents)) // send data to client side
 	}
 
 }
 
 func main() {
-	http.HandleFunc("/", sayhelloName)       // set router
-	err := http.ListenAndServe(":9090", nil) // set listen port
-	if err != nil {
+	config := newrelic.NewConfig("Go ToDo", "909dd7e6d21da9499f9de1c8d5b3aa2995342975")
+	app, err := newrelic.NewApplication(config)
+	txn := app.StartTransaction("todo", nil, nil)
+	http.HandleFunc(newrelic.WrapHandleFunc(app, "/", sayhelloName))
+	http.Handle("/metrics", promhttp.Handler())
+	error := http.ListenAndServe(":9090", nil) // set listen port
+	if error != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+	defer txn.End()
 }
